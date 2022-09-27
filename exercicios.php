71d7992b0e36b7613c5fbf69ca5bc14a05830e2b
ItsukaT2 <78879274+ItsukaT2@users.noreply.github.com>
2022-09-27 11:47:22 -0300

    primeiras alteraçoes

--- exercicios.php
+++ exercicios.php
@@ -22,7 +22,7 @@
    $count++;
   }
 
-  echo "B)";
+echo "B)";
   $letras= ["a","z","m"];
 
   for($i = 0; $i < count($letras); $i++)
@@ -37,23 +37,23 @@
   }
   inverter();
 
-  echo "C)";
-function maior_num(){
-    $_reverse = [1, 2, 3, 101, 5];
-    $maior = 0;
-    foreach($_reverse as $a){
-        echo $a.'<br>';
-        if($a > $maior){
+echo "C)";
+    function maior_num(){
+        $_reverse = [1, 2, 3, 101, 5];
+        $maior = 0;
+        foreach($_reverse as $a){
+            echo $a.'<br>';
+            if($a > $maior){
             $maior = $a;
+            }
         }
-    }
     echo "O maior número é".$maior;
 }
 maior_num()
 
 echo "D)";
-$CPF_LENGTH = 11;
-  $cnpj_cpf = preg_replace("/\D/", '', $value);
+    $CPF_LENGTH = 11;
+        $cnpj_cpf = preg_replace("/\D/", '', $value);
   
   if (strlen($cnpj_cpf) === $CPF_LENGTH) {
     return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
@@ -61,23 +61,23 @@ $CPF_LENGTH = 11;
   return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $cnpj_cpf);
 
 echo "E)";
-Function convert_temp($i){ 
-    $f=$i; 
-    $c= ($f-32)/18; 
-    echo "Fahrenheit :".$f."º, celsius :".$c."º."; 
-    } 
-    convert_temp(90); 
-    echo'<br>'; 
-    convert_temp(77); 
-    echo'<br>'; 
-    convert_temp(52); 
-    echo'<br>'; 
-    convert_temp(12); 
+    Function convert_temp($i){ 
+        $f=$i; 
+        $c= ($f-32)/18; 
+        echo "Fahrenheit :".$f."º, celsius :".$c."º."; 
+        } 
+        convert_temp(90); 
+        echo'<br>'; 
+        convert_temp(77); 
+        echo'<br>'; 
+        convert_temp(52); 
+        echo'<br>'; 
+        convert_temp(12); 
 
 echo "F)";
-function busca_texto1($b){ 
-    echo "Foi encontrada a palavra:"."<p style='color:red;'>".$b."<p style='color:black;'>"; 
-    } 
+    function busca_texto1($b){ 
+        echo "Foi encontrada a palavra:"."<p style='color:red;'>".$b."<p style='color:black;'>"; 
+        } 
     $busca_texto= 'busca_texto1'; 
         $texto=["Rosa","Violeta","Bromelia","Caliandra","Magnolia","Cravo"]; 
         foreach ($texto as $b) { 
@@ -85,11 +85,11 @@ function busca_texto1($b){
             echo "<br>"; 
 
 echo "G)";
-function soma_num(){ 
-    $numeros = [1, 25, 15, 7, 5]; 
-    $menor = 0; 
-    $string="Valores no vetor: ".implode(",",$numeros).'<br>'; 
-    echo $string; 
+    function soma_num(){ 
+        $numeros = [1, 25, 15, 7, 5]; 
+        $menor = 0; 
+        $string="Valores no vetor: ".implode(",",$numeros).'<br>'; 
+        echo $string; 
     foreach($numeros as $a){ 
         if($a > 0){ 
             $soma = array_sum($numeros); 
